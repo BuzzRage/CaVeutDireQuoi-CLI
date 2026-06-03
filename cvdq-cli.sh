effaa715992d2081a87fb6ebbb72f0babdb8ac9b
BuzzRage <buzzrage@protonmail.com>
2026-06-03 14:55:51 +0200

    Improve helper

--- cvdq-cli.sh
+++ cvdq-cli.sh
@@ -17,8 +17,17 @@ verbose=false
 
 # Show Usage
 show_usage(){
-    echo "Usage: $0 ABBR";
-    exit 2;
+    echo -e "Usage : $0 [OPTION]... \033[0;31m ITEM \033[0m";
+    echo -e "\t -h       \t Show this message"
+    echo -e "\t -i \033[0;31m ITEM \033[0m \t Look for \033[0;31m ITEM \033[0m"
+    echo -e "\t -f \033[0;31m FILE \033[0m \t Use \033[0;31m FILE \033[0m as glossary (default : glossaire.json)"
+    echo -e "\t -k \033[0;31m KEY  \033[0m \t Use \033[0;31m KEY \033[0m as attribute to look for (default : desc)"
+    echo -e "\t -v                         \t Enable verbose mode \n"
+    echo -e "List of valid \033[0;31m KEY  \033[0m :"
+    for key in ${keys[@]}; do
+        echo -e "\t \033[0;35m $key \033[0m"
+    done
+    exit 0;
 }
 
 # get attribute
@@ -52,7 +61,7 @@ if [ "$#" -eq 1 ]; then
     ABBR="$1"
 fi
 
-while getopts "vhf:i:k:" opt; do
+while getopts "hf:i:k:v" opt; do
     case $opt in
         f)
             DB=$OPTARG
