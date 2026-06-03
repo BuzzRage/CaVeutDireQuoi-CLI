fb5deda41ed0a512743f6387acdfe6de4a11f393
BuzzRage <buzzrage@protonmail.com>
2026-06-03 12:18:47 +0200

    Add default variables, options handling, verbose mode and file input selection

--- cvdq-cli.sh
+++ cvdq-cli.sh
@@ -2,23 +2,73 @@
 
 # Usage: ./cvdq-cli.sh ABBR
 
-
+### Variables
 # Available keys
 keys=("short_form" "long_form" "desc" "meta" "meta.type" "meta.source" )
-selected_key=${keys[2]}
 
+# Default selected key : desc
+default_selected_key=${keys[2]}
+
+# Default file
+default_file="glossaire.json"
+
+verbose=false
+
+
+# Show Usage
 show_usage(){
     echo "Usage: $0 ABBR";
     exit 2;
 }
 
+# get attribute
+get_attribute(){
+    # jq returns null if key absent; -r to get raw string
+    ATTR=$(jq -r --arg item "$1" --arg key "$selected_key" '.[$item].[$key] // empty' "$2")
 
-if [[ -z "$1" || ( $@ == "--help") ||  $@ == "-h" ]]; then
+    if [ -z "$ATTR" ]; then
+        echo "No description found for '$1'" >&2
+        exit 1
+    fi
+
+    printf "%s\n" "$ATTR"
+}
+
+
+if [ -z "$1" ]; then
     show_usage
 fi
 
+# Default settings
+selected_key=$default_selected_key
+DB=$default_file
+
+
+while getopts "vhf:" opt; do
+    case $opt in
+        v)
+            echo "Verbose mode enabled"
+            verbose=true
+            ;;
+        f)
+            DB=$OPTARG
+            ;;
+        h)
+            show_usage
+            exit 0
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+
+# Attributions
 ABBR="$1"
-DB="glossaire.json"
+
+if [ "$verbose" == true ]; then
+    echo "Input file $DB will be used as a glossary."
+fi
+
 
 if [ ! -f "$DB" ]; then
     echo "Glossary file not found: $DB" >&2
@@ -26,12 +76,5 @@ if [ ! -f "$DB" ]; then
 fi
 
 
-# jq returns null if key absent; -r to get raw string
-DESC=$(jq -r --arg item "$ABBR" --arg key "$selected_key" '.[$item].[$key] // empty' "$DB")
 
-if [ -z "$DESC" ]; then
-    echo "No description found for '$ABBR'" >&2
-    exit 1
-fi
-
-echo "$DESC"
+echo $(get_attribute "$ABBR" "$DB")
