ce55616ce71860860f0773d5a053c99e7e14c401
BuzzRage <buzzrage@protonmail.com>
2026-06-04 18:18:46 +0200

    put get_attribute in a dedicated .sh file in /lib

--- bashly/src/root_command.sh
+++ bashly/src/root_command.sh
@@ -19,14 +19,7 @@ fi
 
 log "$item $file $selected_key"
 
-# jq returns null if key absent (empty) ; -r to get raw string ; split the argument key "meta.type" in an array (splitted by dot '.') as ["meta","type"] ; Then reduce ARRAY[] as $p ( INIT; UPDATE )
-ATTR=$(jq -r --arg item "$item" --arg key $selected_key 'reduce ($key | split("."))[] as $p (.[$item]; if . == null then null else .[$p] end) // empty' "$file")
 
+echo "$item : $(get_attribute)"
 
-if [ -z "$ATTR" ]; then
-    echo "No output found for attribute '$selected_key' for item '$item'" >&2
-    exit 1
-fi
-
-printf "%s\n" "$ATTR"
 
