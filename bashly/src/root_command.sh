bd9d8413949d15b0351f464b7547e48c2d9965d8
BuzzRage <buzzrage@protonmail.com>
2026-06-05 10:25:43 +0200

    Merge legacy code with bashly-generated code

--- bashly/src/root_command.sh
+++ bashly/src/root_command.sh
@@ -11,6 +11,79 @@ declare -g selected_key=${args[--key]}
 declare -g item=${args[item]}
 declare -g file=${args[--file]}
 
+
+## Default selected key : desc 
+## default_selected_key=${keys[2]}
+# # 
+# # 
+# # 
+# # # Default settings
+# # selected_key=$default_selected_key
+# # DB=$default_file
+# # if [ "$#" -eq 1 ]; then
+# #     ABBR="$1"
+# # fi
+# # 
+# # while getopts "hf:i:k:v" opt; do
+# #     case $opt in
+# #         f)
+# #             DB=$OPTARG
+# #             ;;
+# #         h)
+# #             show_usage
+# #             exit 0
+# #             ;;
+# #         i)
+# #             ABBR=$OPTARG
+# #             ;;
+# #         k)
+# #             selected_key=$OPTARG
+# #             ;;
+# #         v)
+# #             verbose=true
+# #             log "Verbose mode enabled"
+# #             ;;
+# #         *)
+# #             show_usage
+# #             exit 1
+# #             ;;
+# #     esac
+# # done
+# # shift $((OPTIND - 1))
+# # 
+# # 
+# # # Verifications
+# # 
+# # if [[ -v ABBR ]]; then
+# #     log "ABBR aldready defined."
+# # else
+# #     log "Item $1 will be searched."
+# #     ABBR=$1
+# # fi
+# # 
+# # 
+# # if [ -f "$DB" ]; then
+# #     log "Input file $DB will be used as a glossary."
+# # else
+# #     log "Error: Glossary file not found: $DB" >&2
+# #     exit 3
+# # fi
+# # 
+# # for key in "${keys[@]}"; do
+# #     if [ "$selected_key" == "$key" ]; then
+# #         log "Selected key $selected_key will be used as a key."
+# #         valid_key=true
+# #     fi
+# # done
+# # 
+# # if [ ! "$valid_key" == true ]; then
+# #     log "Error: $selected_key is not a valid key. Valid keys: $keys"
+# #     exit 2
+# # fi
+# # 
+# # echo "$ABBR : $(get_attribute "$ABBR" "$DB")"
+
+
 if [[ ${args[--verbose]} ]]; then
     verbose=true
 else
